0649d6a0301c3fecb29c03e6033530a8373ab1e7
Season4ki <105760775+Season4ki@users.noreply.github.com>
2025-06-06 17:23:50 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -143,7 +143,7 @@ const config = {
     {go:"🔍 前往",img:"/img/sunshine.jpg",title:"Project 8",subtitle:"8,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://leleo.top",show:false},
   ],
   
-  statement: ["备案号：XXICP备123456789号","Copyright © 2025 Leleo"],
+  statement: ["备案号：XXICP备123456789号","Copyright © 2025 Bamboo.1"],
 }
 
 export default config
